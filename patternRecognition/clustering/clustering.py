16759cb95f9b2949aa98cb9425eb2d45a5efd8f9
JDesq <ju.esro1012@gmail.com>
2020-12-16 18:55:35 -0600

    Fix clustering methods and rebuild dataset (reload)

--- patternRecognition/clustering/clustering.py
+++ patternRecognition/clustering/clustering.py
@@ -15,50 +15,55 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.utils.extmath import randomized_svd 
 
 
-def parseLog(): 
-  filename_in="/media/files/ProyectoDiseno/clustering/Patterns/prueba1.json"
-  with open(filename_in, "r", encoding="utf8") as json_file:
-    data = json.load(json_file)
+def clustering(): 
+  f = open("/media/files/ProyectoDiseno/clustering/Patterns/prueba1.json", "r")
+  content = f.read()
+  jsondecoded = json.loads(content)
+  
+  #classNameData = []
+  #packageData = []
+  #modifiersAtributesData = []
+  #modifiersMethodsData = []
+  content_list = []
+  for x in range(0,len(jsondecoded)):
+    data1 = jsondecoded[x]
+    string_content = ""
+    string_content = string_content + "class name " + str(data1["className"] + " ")
+    #classNameData.append(data1["className"]) #AST class name.
+    string_content = string_content + "package name " + str(data1["package"] + " ")
+    #packageData.append(data1["package"]) #AST package name.
+    for entity in data1["atributes"]: 
+      for entityModifier in entity["modifier"]:
+        string_content = string_content + "modifier atribute " + str(entityModifier) + " "
+        #modifiersAtributesData.append(entityModifier) # AST modifiers atributes type.
+    for entity2 in data1["methods"]:
+      for entityModifier2 in entity2["modifier"]:
+        string_content = string_content + "modifier method " + str(entityModifier2) + " "
+        #modifiersMethodsData.append(entityModifier2) # AST modifiers methods type.
+    content_list.append(string_content)
+  #print(classNameData)
+  #print(packageData)
+  #print(modifiersAtributesData)
+  #print(modifiersMethodsData)
+  #print(content_list)
+  news_df = pd.DataFrame(columns=content_list)
+  news_df.to_csv('data3.csv', index=False)
 
-  #First format
-  news_df2 = pd.DataFrame({'document':data})
-  news_df2.to_csv('data2.csv', index=False)
-  
-  #Atributes information
-  for content in data:
-    atributes = content.pop("atributes")
-    content["atribute_modifiers"] = atributes[0]["modifier"]
-    content["atribute_name"] = atributes[0]["name"]
-    content["atribute_type"] = atributes[0]["type"]
-  
-  #Methods information
-  for content1 in data:
-    methods = content1.pop("methods")
-    content1["methods_modifiers"] = methods[1]["modifier"]
-    content1["methods_name"] = methods[1]["name"]
-    content1["methods_arguments"] = methods[0]["arguments"]
-    content1["methods_returnType"] = methods[1]["returnType"]
-  
-  #Second format
-  news_df = pd.DataFrame(data)
-  news_df.to_csv('data.csv', index=False)
-  #print(len(d.columns))
-  
   
   # tfidf vectorizer of scikit learn
-  vectorizer = TfidfVectorizer(max_features=1000, max_df = 0.5, use_idf = True, ngram_range=(1,3))
+  vectorizer = TfidfVectorizer(max_features=100, max_df = 0.5 ,use_idf = True, ngram_range=(1,3))
   X = vectorizer.fit_transform(news_df)
   print(X.shape) # check shape of the document-term matrix
   terms = vectorizer.get_feature_names()
 
-  num_clusters = 3
+  num_clusters = 2
   km = KMeans(n_clusters=num_clusters)
   km.fit(X)
   clusters = km.labels_.tolist()
 
   # applying lsa //////////////////////////////
 
-  U, Sigma, VT = randomized_svd(X, n_components=3, n_iter=100, random_state=122)
+  U, Sigma, VT = randomized_svd(X, n_components=10, n_iter=100, random_state=122)
   #printing the concepts
   for i, comp in enumerate(VT):
         terms_comp = zip(terms, comp)
@@ -69,13 +74,13 @@ def parseLog():
         print(" ")
   
   X_topics=U*Sigma
-  embedding = umap.UMAP(n_neighbors=3, min_dist=0.5, random_state=12).fit_transform(X_topics)
+  embedding = umap.UMAP(n_neighbors=50, min_dist=0.5, random_state=12).fit_transform(X_topics)
   plt.figure(figsize=(7,5))
-  plt.scatter(embedding[:, 0], embedding[:, 1], c = clusters, s = 10, edgecolor='none') # s = size
+  plt.scatter(embedding[:, 0], embedding[:, 1], c = clusters, s = 20, edgecolor='none') # s = size
   plt.show()
   
   print("done")
 
 
 if __name__ == "__main__":
-    parseLog()
+    clustering()
--- patternRecognition/clustering/prueba1.py
+++ patternRecognition/clustering/prueba1.py
@@ -12,10 +12,40 @@ def parseLog():
   filename_in="/media/files/ProyectoDiseno/clustering/Patterns/prueba1.json"
   with open(filename_in, "r", encoding="utf8") as json_file:
     data = json.load(json_file)
-  print(data)
+  #print(data)
   
-
-
+  f = open("/media/files/ProyectoDiseno/clustering/Patterns/prueba1.json", "r")
+  content = f.read()
+  jsondecoded = json.loads(content)
+  
+  #classNameData = []
+  #packageData = []
+  #modifiersAtributesData = []
+  #modifiersMethodsData = []
+  content_list = []
+  for x in range(0,len(jsondecoded)):
+    data1 = jsondecoded[x]
+    string_content = ""
+    string_content = string_content + "class name " + str(data1["className"] + " ")
+    #classNameData.append(data1["className"]) #AST class name.
+    string_content = string_content + "package name " + str(data1["package"] + " ")
+    #packageData.append(data1["package"]) #AST package name.
+    for entity in data1["atributes"]: 
+      for entityModifier in entity["modifier"]:
+        string_content = string_content + "modifier atribute " + str(entityModifier) + " "
+        #modifiersAtributesData.append(entityModifier) # AST modifiers atributes type.
+    for entity2 in data1["methods"]:
+      for entityModifier2 in entity2["modifier"]:
+        string_content = string_content + "modifier method " + str(entityModifier2) + " "
+        #modifiersMethodsData.append(entityModifier2) # AST modifiers methods type.
+    content_list.append(string_content)
+  #print(classNameData)
+  #print(packageData)
+  #print(modifiersAtributesData)
+  #print(modifiersMethodsData)
+  print(len(content_list))
+  news_df = pd.DataFrame()
+  news_df.to_csv('data3.csv', index=False)
 
 
 def prueba1():
